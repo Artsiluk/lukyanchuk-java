0d1ebc9c2965a603623c6018bffd7a6da4a58ff4
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-18 09:08:35 +0300

    Update Task5.java

--- src/main/java/project/homework/homework3/Task5.java
+++ src/main/java/project/homework/homework3/Task5.java
@@ -7,6 +7,7 @@ public class Task5 {
 
     public static void main(String[] args) {
         for (int i = 2; i <= 100; i++) {
+            //а можно просто i+=2
             if (i % 2 == 0) {
                 System.out.println(i);
             }
