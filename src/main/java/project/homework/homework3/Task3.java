771baf45133f975d5b78e6bf99daf7ff8d3f10b8
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-18 09:07:51 +0300

    Update Task3.java

--- src/main/java/project/homework/homework3/Task3.java
+++ src/main/java/project/homework/homework3/Task3.java
@@ -2,13 +2,13 @@ package project.homework.homework3;
 /*
 Составьте программу, вычисляющую A*B, не пользуясь операцией умножения.
  */
+//+
 public class Task3 {
     public static void main(String[] args) {
 
         int a = 4;
         int b = 3;
         int result = 0;
-
         for (int i = 1; i <= b ; i++) {
             result += a;
         }
