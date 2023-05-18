1d92a3be8c1667e424b7b2f8ba8a6e0c97c90966
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-18 09:06:24 +0300

    Update Task1.java

--- src/main/java/project/homework/homework3/Task1.java
+++ src/main/java/project/homework/homework3/Task1.java
@@ -4,6 +4,7 @@ package project.homework.homework3;
 Одноклеточная амеба каждые 3 часа делится на 2 клетки. Определить,
 сколько амеб будет через 3, 6, 9, 12,..., 24 часа.
  */
+//+
 public class Task1 {
     public static void main(String[] args) {
         int ameba = 1;
